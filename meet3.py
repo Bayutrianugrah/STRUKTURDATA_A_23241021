7660d8d89c48d7e267acb236f718974afbbdadea
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-05-08 19:09:19 +0800

    Add files via upload
    PERTEMUAN 3

---
+++ meet3.py
@@ -0,0 +1,13 @@
+# belajar tipe data
+
+# tipe data integer : bilangan bulat
+bil_integer = 100
+print (type(bil_integer))
+
+print ("tipe data dari bil_integer adalah :",(type(bil_int)))
+
+# tipe data float : bilangan desimal
+ bil_float = 2.5
+ print (type(bil_float))
+
+print ("tipe data dari bil_float adalah :",(type(bil_float)))
