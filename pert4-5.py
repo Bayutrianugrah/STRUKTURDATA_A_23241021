4d910129e1e25cc983bc1a45f9b372f014fa383c
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-05-08 19:31:20 +0800

    Add files via upload
    pertemuan 4-5

---
+++ pert4-5.py
@@ -0,0 +1,41 @@
+# inputan
+# aritmatika + - x :
+
+x = 5
+# coba = input("masukan nama :")
+# print(coba)
+
+# bilangan_random = input("masukan bilangan acak :")
+# print(bilangan_random)
+
+# aritmatika
+x = 5
+y = 3
+
+# PENJUMLAHAN +
+hasil = x + y
+print(x, "+" ,y, "=", hasil)
+
+# PERKALIAN *
+hasil = x + y
+print(x, "+" ,y, "=", hasil)
+
+# PEMBAGIAN /
+hasil = x / y
+print(x, "/" ,y, "=", hasil)
+
+# MODULUS %
+hasil = x % y
+print(x, "%" ,y, "=", hasil)
+
+# SKALA PRIORITAS
+x = 3
+y = 4
+z = 5
+
+hasil = x + y * z
+print(x, "+" , y, "*", z, "=" , hasil)
+
+
+
+
