a52b33db6e112de9297f339419ce7c8889a2a6ff
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-05-14 12:17:46 +0800

    Add files via upload
    pertemuan 6

---
+++ meet6.py
@@ -0,0 +1,55 @@
+# perbandingan >, <, =, <= ==, !=, is, isnot
+
+x = 3
+y = 4
+
+# lebih besar >
+hasil = y > x
+print (x, ">", y, "=", hasil)
+
+# lebih kecil <
+hasil = y < x
+print (x, "<", y, "=", hasil)
+
+# lebih besar >=
+hasil = y >= x
+print (x, ">=", y, "=", hasil)
+print (x, ">=", 3, "=", hasil)
+
+
+# lebih besar <=
+hasil = y >= x
+print (x, "<=", y, "=", hasil)
+print (x, "<=", 3, "=", hasil)
+
+# is (adalah)
+hasil = x is 9
+print(x, "is", 9, "=", hasil)
+
+# ==
+a = 5
+b = 5
+c = 10
+hasil = a == b
+print (a, "==", b, "=", hasil)
+print (a, "==", 5, "=", hasil)
+hasil = a == c
+print (a, "==", c, "=", hasil)
+print (a, "==", 10, "=", hasil)
+
+# !=
+a = 5
+b = 5
+c = 10
+hasil = a != b
+print (a, "!=", b, "=", hasil)
+print (a, "!=", 5, "=", hasil)
+hasil = a != c
+print (a, "!=", c, "=", hasil)
+print (a, "!=", 10, "=", hasil)
+
+
+
+
+
+
