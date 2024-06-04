606d3b4fb416af833188b663c49deeca13d64c52
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-06-04 11:43:59 +0800

    Add files via upload
    selasa 4 juni 2024

---
+++ meet8.py
@@ -0,0 +1,24 @@
+# Belajar percabangan
+
+nama = input("masukan nama :")
+
+#if kondisi : aksi (standar)
+#if nama=="edy": print("dosen ganteng")
+#print ("program selsai")
+
+#if kondisi : aksi (indentasi)
+#if nama=="edy":
+    #print("dosen ganteng") 
+    #print("ganteng banget")
+#print ("program selsai")
+
+if nama=="edy":
+    print("dosen ganteng") 
+    print(f"ganteng banget,makasih {nama}")
+else:
+    print("ah gak ganteng")    
+print (f"program selsai, thaks {nama}")
+    
+
+
+
