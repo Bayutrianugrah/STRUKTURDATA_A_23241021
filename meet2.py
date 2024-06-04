87cf22137008f5ecc1fb98609c7886f469497263
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-06-04 11:53:45 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,13 @@
+# hari ini kita belajar variabel
+#assigmen value
+
+x = 3 #assigmen pertama
+y = 9
+print ("nilai UAS saya adalah ", y)
+print ("nilai UTS saya adalah ", x)
+
+jumlah_a = 100 # tidak boleh spasi
+bungkus10 = 5000  # tidak bole di awali angka
+
+x = 100  #assigment kedua
+print("nilai UAS2 saya adalah" , x)
