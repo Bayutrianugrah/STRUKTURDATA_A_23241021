14dccb6e7ffc74b75927a6b418d9d9a4a4123c5d
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2024-06-25 11:51:04 +0800

    Add files via upload

---
+++ meet 10-11.py
@@ -0,0 +1,37 @@
+# belajar perulangan (loop)
+
+# bil = 1
+# print(bil)
+# bil = bil + 2
+# print(bil)
+# bil = bil + 2
+# print(bil)
+# bil = bil + 2
+# print(bil)
+
+# for kondisi :
+#  aksi
+
+# penggunaan loop pada  pemerograman lain
+# for (int i; i<=10; i++) {
+#  aksi
+#  aksi
+# }
+
+# perulangan menggunakan list
+bil = [0,1,11,3,9]
+print(bil)
+
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
+print("akhir program")
+
+# perulangan menggunakan range
+bil2 = range(10)
+for i in bil2 :
+    #print("pak edy dosen ganteng -> {i}")
+print("akhir program 2")
+
+# for bil in range(10) :
+#  aksi
+#  aksi
